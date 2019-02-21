4b26933ace73acc4c6ae4b36afa581ff8e167107
Rahul D <rahul.pragma@gmail.com>
2019-02-21 17:50:19 +0530

    Rahul: Updates

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 $to = "rahul@example.com";
-$subject = "My subject";
+$subject = "This is my subject";
 $txt = "Hello world!";
 $headers = "From: webmaster@example.com" . "\r\n" .
-"CC: pankaj@example.com, dinesh@example.com";
-
+"CC: pankaj@example.com, dinesh@example.com, nupendra@example.com";
 
 if(mail($to,$subject,$txt,$headers))
 {
 	echo "Email sent";
 }
+
+mail($to,$subject,$txt,$headers)
+echo "Message Sent"
+
 ?>
